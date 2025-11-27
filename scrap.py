2e0f2e25d8402c5b6b17faeee4bc0600fc10ed05
Lucasdelpiero <lucasdelpiero98@gmail.com>
2025-11-27 09:36:08 -0300

    Funciona con multiples links

--- scrap.py
+++ scrap.py
@@ -20,23 +20,41 @@ def make_card(name, icon, image):
     card = Card(name, icon, image)
     return card
 
-url = "https://dragonball.center/catalogo/coleccion/1221/dragon-ball-z-juego-de-cartas-coleccionables"
-result = requests.get(url)
+urls = ["https://dragonball.center/?load=dbc&catalogo&coleccion=500_dragon-ball-z-juego-de-cartas-coleccionables&grupo=734",
+        "https://dragonball.center/?load=dbc&catalogo&coleccion=500_dragon-ball-z-juego-de-cartas-coleccionables&grupo=735",
+        "https://dragonball.center/?load=dbc&catalogo&coleccion=500_dragon-ball-z-juego-de-cartas-coleccionables&grupo=736",
+        "https://dragonball.center/?load=dbc&catalogo&coleccion=500_dragon-ball-z-juego-de-cartas-coleccionables&grupo=737",
+        "https://dragonball.center/?load=dbc&catalogo&coleccion=500_dragon-ball-z-juego-de-cartas-coleccionables&grupo=738",
+        "https://dragonball.center/?load=dbc&catalogo&coleccion=500_dragon-ball-z-juego-de-cartas-coleccionables&grupo=740",
+        "https://dragonball.center/?load=dbc&catalogo&coleccion=500_dragon-ball-z-juego-de-cartas-coleccionables&grupo=741",
+        "https://dragonball.center/?load=dbc&catalogo&coleccion=500_dragon-ball-z-juego-de-cartas-coleccionables&grupo=742",
+        "https://dragonball.center/?load=dbc&catalogo&coleccion=500_dragon-ball-z-juego-de-cartas-coleccionables&grupo=743",
+        "https://dragonball.center/?load=dbc&catalogo&coleccion=500_dragon-ball-z-juego-de-cartas-coleccionables&grupo=744",
+        "https://dragonball.center/?load=dbc&catalogo&coleccion=500_dragon-ball-z-juego-de-cartas-coleccionables&grupo=745",
+        "https://dragonball.center/?load=dbc&catalogo&coleccion=500_dragon-ball-z-juego-de-cartas-coleccionables&grupo=746",
+        "https://dragonball.center/?load=dbc&catalogo&coleccion=500_dragon-ball-z-juego-de-cartas-coleccionables&grupo=747",
+        "https://dragonball.center/?load=dbc&catalogo&coleccion=500_dragon-ball-z-juego-de-cartas-coleccionables&grupo=748",
+        "https://dragonball.center/?load=dbc&catalogo&coleccion=500_dragon-ball-z-juego-de-cartas-coleccionables&grupo=749",
+        "https://dragonball.center/?load=dbc&catalogo&coleccion=500_dragon-ball-z-juego-de-cartas-coleccionables&grupo=750"
+]
+cont = 10
+for url in urls:
+    cont = cont + 1
+    result = requests.get(url)
 
-doc = BeautifulSoup(result.text, "html.parser")
-objetos = doc.find_all("div", class_= "catalogo_coleccion_objeto")
+    doc = BeautifulSoup(result.text, "html.parser")
+    objetos = doc.find_all("div", class_= "catalogo_coleccion_objeto")
 
-#print(doc.prettify())
-cards = []
+    #print(doc.prettify())
+    cards = []
 
-# Crea cartas usando la data de la pagina
-for obj in objetos:
-    hijos = obj.find_all("div")     # o el nombre del tag que necesites
-    nombre = ""
-    icon = ""
-    image = ""
-    for hijo in hijos:
-        if hijo:
+    # Crea cartas usando la data de la pagina
+    for obj in objetos:
+        hijos = obj.find_all("div")     # o el nombre del tag que necesites
+        nombre = ""
+        icon = ""
+        image = ""
+        for hijo in hijos:
             clase = hijo.get("class")[0]
             if clase == INFO:
                 s = hijo.find("span")
@@ -54,57 +72,50 @@ for obj in objetos:
 
                 image = hijo.get("onclick")
                 image = "https://dragonball.center" + image.split("visor('")[1].split(",")[0].strip("'")
-    cards.append(make_card(nombre, icon, image))             
+        cards.append(make_card(nombre, icon, image))             
 
+    # Crear carpetas si no existen
+    os.makedirs("iconos/" + str(cont), exist_ok=True)
+    os.makedirs("imagenes/" + str(cont), exist_ok=True)
 
-for card in cards:
-    print(card.name)
-    print(card.icon)
-    print(card.image)
-    print()
+    # Descarga imagenes e iconos
+    for card in cards:
+        # DESCARGA IMAGENES
+        print(f"Descargando imagen {card.name}...")
 
-# Crear carpetas si no existen
-os.makedirs("iconos", exist_ok=True)
-os.makedirs("imagenes", exist_ok=True)
+        response = requests.get(card.image)
 
-# Descarga imagenes e iconos
-for card in cards:
-    # DESCARGA IMAGENES
-    print(f"Descargando imagen {card.name}...")
+        if response.status_code == 200:
+            # Guardar como nombre.jpg (o lo que corresponda)
+            extension = card.image.split('.')[-1]
+            filename = f"{card.name}.{extension}"
 
-    response = requests.get(card.image)
+            nombre = f"{card.name}.jpg"
+            with open("imagenes/" + os.path.join(str(cont) , nombre), "wb") as f:
+                f.write(response.content)
 
-    if response.status_code == 200:
-        # Guardar como nombre.jpg (o lo que corresponda)
-        extension = card.image.split('.')[-1]
-        filename = f"{card.name}.{extension}"
+            print(f"✔ Guardado: {filename}")
+        else:
+            print(f"✖ Error descargando {card.icon}")
+        
 
-        nombre = f"{card.name}.jpg"
-        with open(os.path.join("imagenes", nombre), "wb") as f:
-            f.write(response.content)
+        # DESCARGA ICONOS
+        print(f"Descargando icono {card.name}...")
 
-        print(f"✔ Guardado: {filename}")
-    else:
-        print(f"✖ Error descargando {card.icon}")
-    
+        response = requests.get(card.icon)
 
-    # DESCARGA ICONOS
-    print(f"Descargando icono {card.name}...")
+        if response.status_code == 200:
+            # Guardar como nombre.jpg (o lo que corresponda)
+            extension = card.icon.split('.')[-1]
+            filename = f"{card.name}.{extension}"
 
-    response = requests.get(card.icon)
+            nombre = f"{card.name}.jpg"
+            with open("iconos/" + os.path.join(str(cont), nombre), "wb") as f:
+                f.write(response.content)
 
-    if response.status_code == 200:
-        # Guardar como nombre.jpg (o lo que corresponda)
-        extension = card.icon.split('.')[-1]
-        filename = f"{card.name}.{extension}"
-
-        nombre = f"{card.name}.jpg"
-        with open(os.path.join("iconos", nombre), "wb") as f:
-            f.write(response.content)
-
-        print(f"✔ Guardado: {filename}")
-    else:
-        print(f"✖ Error descargando {card.icon}")
+            print(f"✔ Guardado: {filename}")
+        else:
+            print(f"✖ Error descargando {card.icon}")
 
 
 
